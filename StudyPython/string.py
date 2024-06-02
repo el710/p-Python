6b802d7005dc58cd7a4f024e7a9d68cfc9b26eca
el710 <el710@mail.ru>
2024-06-02 19:31:32 +0300

    homework 1

---
+++ StudyPython/string.py
@@ -0,0 +1,23 @@
+import os
+os.system('cls')
+
+### https://habr.com/ru/companies/ruvds/articles/500296/ - 41 вопрос о работе со строками в Python
+
+
+print('Here is about strings & print()')
+
+string_a = 'Hello'
+string_b = "World"
+string_c = '"quots"' # or "'quots'"
+
+print(string_a, string_b, string_c)
+print("Hello, " + string_b)
+print("hello" * 4)
+print(string_a[0], string_a[-1])
+
+#срез
+print(string_b[0:3])  ## begin(def = 0) : last(ex def = len) : step
+
+print(string_b[0:5:2])
+
+print(string_b[::-1])
