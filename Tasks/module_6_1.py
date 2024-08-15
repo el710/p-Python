f9fd48ed8c6dc70047e917d7d5c4deaae0083783
el710 <el710@mail.ru>
2024-08-15 18:52:21 +0300

    work 6-1 rebuild

--- Tasks/module_6_1.py
+++ Tasks/module_6_1.py
@@ -6,33 +6,22 @@ from random import randint
 from time import sleep
 
 
-class Bio():
+class Animal():
     def __init__(self, name) -> None:
-        self.name = name
-    
-    def __repr__(self) -> str:
-        return f"class <Bio> name: '{self.name}'"
-
-class Animal(Bio):
-    def __init__(self, name) -> None:
-        super().__init__(name)
         self.alive = True
         self.fed = False
+        self.name = name
     
     def __str__(self) -> str:
         return f"animal {self.name}"
     
     def eat(self, food):
-        if isinstance(food, Fruit):
+        if food.edible:
             print(f"{self.name} has eaten {food.name}")
             self.fed = True
-        elif isinstance(food, Flower):
-            if isinstance(self, Herbivore) and food.edible:
-                print(f"{self.name} has eaten {food.name}")
-                self.fed = True
-            else:
-                print(f"{self.name} hasn't eat {food.name}")
-                self.alive = False
+        else:
+            print(f"{self.name} hasn't eat {food.name}")
+            self.alive = False
 
     def state(self):
         if self.alive:
@@ -41,16 +30,16 @@ class Animal(Bio):
             return f"{self.name} is dead"
     
 
-class Plant(Bio):
-    edible = False
+class Plant():
     def __init__(self, name) -> None:
-        super().__init__(name)
+        self.edible = False
+        self.name = name
 
-class Herbivore(Animal):
+class Mammal(Animal):
     def __init__(self, name) -> None:
         super().__init__(name)
     
-class Preditor(Animal):
+class Predator(Animal):
     def __init__(self, name) -> None:
         super().__init__(name)
 
@@ -68,38 +57,18 @@ class Fruit(Plant):
 if __name__ == '__main__':
     system('cls')
 
-    # bio_object = Bio('anylive')
-    # print(bio_object.__repr__())
+    a1 = Predator('Bear')
+    a2 = Mammal('Hatiko')
+    p1 = Flower('Romashko')
+    p2 = Fruit('Orange')
 
-    # animal = Animal('anime')
-    # print(animal)
+    print(a1.name)
+    print(p1.name)
 
-    cow = Herbivore('Cow')
-    bear = Preditor('Bear')
-    flora = [Flower('Hydrangea'), Fruit('Apple'), Flower('Atropa'), Fruit('Ananas')]
+    print(a1.alive)
+    print(a2.fed)
 
-
-    print(f'''{cow} and {bear}
-          have field of destiny:''')
-    for i in flora:
-        print(i)
-
-    print("Let's go...\n")
-    challenger = cow
-    while cow.alive and bear.alive and len(flora) > 0:
-        chance = randint(0, len(flora) - 1)
-        food = flora.pop(chance)
-
-        print(f"{challenger.name} made a step and met {food}")
-        challenger.eat(food)
-        sleep(1)
-
-        if isinstance(challenger, Herbivore):
-            challenger = bear
-        else:
-            challenger = cow        
-    
-    print(cow.state())
-    print(bear.state())
-    
-    input("\n press <Enter> to leave...")
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
