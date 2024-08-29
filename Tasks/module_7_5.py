9a2a4861f02f1cf2073f453b89d0f70a38773bb5
el710 <el710@mail.ru>
2024-08-29 18:18:43 +0300

    work 7-5 +

---
+++ Tasks/module_7_5.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+os.system('cls')
+
+os.chdir('StudyPython')
+directory = os.curdir
+print(f"\n Start tree directory: {directory}")
+
+
+for root, dirs, files in os.walk(directory): ## walk through directory's tree
+    for file in files:
+        full_path = os.path.join(directory, root, file)
+        raw_modify_time = os.path.getmtime(full_path)
+        modify_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(raw_modify_time))
+        
+        print(f"""Found file: {file}
+            Path: {full_path}
+            Size: {os.path.getsize(full_path)} bytes
+            Modify time: {modify_time}
+            Root dir: {os.path.dirname(full_path)}
+            """)
