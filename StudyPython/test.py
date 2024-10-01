3726d094d7ec3d99fa3f308ab7bf331d9464b8aa
el710 <el710@mail.ru>
2024-10-01 18:43:12 +0300

    home work 10-4

--- StudyPython/test.py
+++ StudyPython/test.py
@@ -1,55 +1,100 @@
-from threading import Thread, Lock
 from random import randint
+from threading import Thread
 from time import sleep
+from queue import Queue
 
-TRANS_COUNT = 100
-TRANS_MIN = 50
-TRANS_MAX = 500
-TRANS_TIMEOUT = 0.001
+BUSY_MIN = 3
+BUSY_MAX = 10
 
-class Bank(Thread):
-    def __init__(self) -> None:
-        super().__init__()
-        self.balance = 0
-        self.lock = Lock()
+class Table():
+    def __init__(self, number, guest=None) -> None:
+        self.number = number
+        self.guest = guest
+      
+    def put_guest(self, guest) -> bool:
+        if self.guest == None:
+            self.guest = guest
+            return True
+        return False
+        
+    def free(self):
+        self.guest = None
     
-    def deposit(self, transaction):
-        for i in range(transaction):
-            value = randint(TRANS_MIN, TRANS_MAX)
-            self.balance += value
-            print(f'{i} Deposit: <{value}>. Balance: {self.balance}')
-            if self.balance >= TRANS_MAX and self.lock.locked():
-                self.lock.release()
-                # print('---deposit(): lock released')
-            sleep(TRANS_TIMEOUT)
+    def is_free(self):
+        return self.guest == None
+    
+    def get_number(self):
+        return self.number
+    
+    def get_guest(self):
+        return self.guest
 
-    def take(self, transaction):
-        for i in range(transaction):
-            value = randint(TRANS_MIN, TRANS_MAX)
-            print(f'{i} Request: <{value}>...')
-            if value <= self.balance:
-                self.balance -= value
-                print(f'{i} Take: <{value}>. Balance: {self.balance}')
-            else:
-                print(f'{i} Request rejected: not enoght balance')
-                # print(f'---take({i}): acquire lock...')
-                self.lock.acquire()
-                # print(f'---take({i}): lock passed')
+class Guest():
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+    
+    def run(min, max):
+        sleep(randint(min, max))
 
 
-bk = Bank()
-th1 = Thread(target=bk.deposit, args=(TRANS_COUNT, ))
-th2 = Thread(target=bk.take, args=(TRANS_COUNT, ))
+class Cafe():
+    def __init__(self, *tables) -> None:
+        self.queue = Queue()
+        self.tables = tables
+        self.threads = {}
+      
+    def __del__(self):
+        for key, thread in self.threads.items():
+            thread.join()  
+    
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            guest_wait = True
+            for table in self.tables:
+                if table.put_guest(guest):
+                  print(f'{guest} has got table N {table.get_number()}')
+                  thread = Thread(target=Guest.run, args=(BUSY_MIN, BUSY_MAX, ))
+                  thread.start()
+                  self.threads[guest] = thread
 
-th1.start()
-th2.start()
+                  guest_wait = False
+                  break
+            if guest_wait:
+                  self.queue.put(guest)
+                  print(f'{guest} in line...')
+                  
+    def discuss_guest(self):
+        busy_tables = True
+        while not self.queue.empty() or busy_tables:
+            for table in self.tables:
+                if not table.is_free():
+                    guest = table.get_guest()
+                    if not self.threads[guest].is_alive():
+                        print(f'{guest} have done and gone')
+                        print(f'Table {table.get_number()} is free')
+                        table.free()
 
-th1.join()
-th2.join()
+                if table.is_free() and not self.queue.empty():
+                    guest = self.queue.get()
+                    table.put_guest(guest)
+                    print(f'{guest} has out from line and got table N {table.get_number()}')
+                    thread = Thread(target=Guest.run, args=(BUSY_MIN, BUSY_MAX, ))
+                    thread.start()
+                    self.threads[guest] = thread
+            
+            busy_tables = False
+            for table in self.tables:
+                if not table.is_free():
+                    busy_tables = True
 
-print(f' State of balance: {bk.balance}')
 
+tables = [Table(number) for number in range(1, 6)]
+guest_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman', 'Victoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra']
+guests = [Guest(name) for name in guest_names]
 
-
-
-
+cafe = Cafe(*tables)
+cafe.guest_arrival(*guests)    
+cafe.discuss_guest()
