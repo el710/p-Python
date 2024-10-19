8d757d7ac29d45645804d7e258aa366726589d85
el710 <el710@mail.ru>
2024-10-19 23:40:11 +0300

    work 12-2 +

--- Tasks/test_12_2.py
+++ Tasks/test_12_2.py
@@ -10,7 +10,7 @@ class TournamentTest(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         print("setUpClass(): let's start testing")
-        cls.all_results = {}
+        cls.all_results = []
 
     def setUp(self):
         self.runner_1 = Runner('Usein', 10)
@@ -20,29 +20,30 @@ class TournamentTest(TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         print("tearDownClass(): testing is over")
+        for item in cls.all_results:
+            print(item)
 
-
-    def tearDown(self):
-        for item in TournamentTest.all_results:
-            print(item, TournamentTest.all_results[item])
     
     def test_tour1(self):
         tournament = Tournament(90, self.runner_1, self.runner_3)
-        TournamentTest.all_results = tournament.start()
-        print('\n', TournamentTest.all_results)
-        self.assertTrue(TournamentTest.all_results, 'Nick')
+        res = tournament.start()
+        ind = len(res)
+        TournamentTest.all_results.append(res)    
+        self.assertTrue(TournamentTest.all_results[0][ind]=='Nick')
 
     def test_tour2(self):
         tournament = Tournament(90, self.runner_2, self.runner_3)
-        TournamentTest.all_results = tournament.start()
-        print('\n', TournamentTest.all_results)
-        self.assertTrue(TournamentTest.all_results, 'Nick')
+        res = tournament.start()
+        ind = len(res)
+        TournamentTest.all_results.append(res)    
+        self.assertTrue(TournamentTest.all_results[1][ind]=='Nick')
 
     def test_tour3(self):
         tournament = Tournament(90, self.runner_1, self.runner_2, self.runner_3)
-        TournamentTest.all_results = tournament.start()
-        print('\n', TournamentTest.all_results)
-        self.assertTrue(TournamentTest.all_results, 'Nick')
+        res = tournament.start()
+        ind = len(res)
+        TournamentTest.all_results.append(res)    
+        self.assertTrue(TournamentTest.all_results[2][ind]=='Nick')
 
    
 
