20292a6d3691e9ce41456dde782d27df5ce275ce
el710 <el710@mail.ru>
2024-10-30 15:34:45 +0300

    work 14-2

--- StudyPython/SQLBase/main.py
+++ StudyPython/SQLBase/main.py
@@ -36,8 +36,8 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 """ ###                              which columns           maket of data           tuple of means
 # cursor.execute("INSERT INTO Users (username, email, age) VALUES(?, ?, ?)", ("newuser", "ex@gmail.com", "25"))
 
-for i in range(30):
-    cursor.execute("INSERT INTO Users (username, email, age) VALUES(?, ?, ?)", (f"newuser_{i}", f"ex_{i}@gmail.com", random.randint(25, 50)))
+# for i in range(30):
+#     cursor.execute("INSERT INTO Users (username, email, age) VALUES(?, ?, ?)", (f"newuser_{i}", f"ex_{i}@gmail.com", random.randint(25, 50)))
 
 """
     Read data...
@@ -50,10 +50,37 @@ cursor.execute("SELECT * FROM Users")
 # cursor.execute("SELECT username, age FROM Users GROUP BY age")
 # cursor.execute("select username, age from Users group by age") -- also works
 
-
 users = cursor.fetchall()
 for user in users:
     print(f"{user} - {type(user)}")
+
+"""
+    take count of records...
+"""
+cursor.execute("Select Count(*) From Users")
+total_1 = cursor.fetchone()[0]
+print(f"Rec count: {total_1}")
+
+## how much records with age > 28
+# cursor.execute("Select Count(*) From Users Where age > ?", (28, ))
+
+## middle age...
+cursor.execute("Select SUM(age) From Users")
+sum_age = cursor.fetchone()[0]
+cursor.execute("Select COUNT(*) From Users")
+all_count = cursor.fetchone()[0]
+print(f"Mid age : {sum_age / all_count}")
+## check out...
+cursor.execute("Select AVG(age) From Users")
+avg_age = cursor.fetchone()[0]
+print(f"Avg age : {avg_age}")
+
+## min - max
+cursor.execute("Select Min(age) From Users")
+min_age = cursor.fetchone()[0]
+cursor.execute("Select Max(age) From Users")
+max_age = cursor.fetchone()[0]
+print(f"Min-Max: {min_age} - {max_age}")
 
 
 """
@@ -65,6 +92,8 @@ for user in users:
     Delete records...
 """
 # cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser", ))
+
+
 
 
 
